cd254cfb0cfc53120cec74be7871aebedcd12507
nabiilahputri13 <putrisafa1394@gmail.com>
2024-02-24 21:38:39 +0700

    apply solid

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomepageController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomepageController.java
@@ -1,12 +1,17 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomepageController {
-    @GetMapping("/")
-    public String homepage() {
-        return "homepage";
+
+    @GetMapping("")
+    public String homePage(Model model){
+        return "homePage";
     }
+
 }
