cf65c644a365c2450d3c667fd7e0596434d2bf8c
nabiilahputri13 <putrisafa1394@gmail.com>
2024-03-06 16:25:23 +0700

    [GREEN] Implement Payment model.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 
 import java.util.*;
 
-@Builder
+
 @Getter
 public class Payment {
     String id;
@@ -14,12 +14,84 @@ public class Payment {
     Map<String, String> paymentData;
     Order order;
 
-    @Setter
     String status;
 
     public Payment(String id, String method, Map<String, String> paymentData, Order order) {
+        this.id = id;
+        this.paymentData = paymentData;
+        this.method = method;
+        this.order = order;
+
+        if (method.equals("VOUCHER_CODE")) {
+            this.status = verifyCode();
+        }
+        else if (method.equals("BANK_TRANSFER")) {
+            this.status = verifyTransfer();
+        }
+        else {
+            throw new IllegalArgumentException("Payment method not valid");
+        }
+
+        if (status==null) {
+            updateStatus();
+        }
+
+        if (order == null) {
+            throw new IllegalArgumentException("Order cannot be null");
+        }
+
+        if (method == null || method.isEmpty()) {
+            throw new IllegalArgumentException("Payment method type cannot be empty");
+        }
+
+        if (paymentData == null || paymentData.isEmpty()) {
+            throw new IllegalArgumentException("Payment data cannot be empty");
+        }
     }
 
-    public Payment(String id, String method, Map<String, String> paymentData, Order order, String status) {
+    public String verifyCode() {
+        String voucherCode = this.paymentData.get("voucherCode");
+
+        if (voucherCode.length()!=16) {
+            return "REJECTED";
+        }
+        else if (!voucherCode.startsWith("ESHOP")) {
+            return "REJECTED";
+        }
+        int num = 0;
+        for (int i=0; i<voucherCode.length(); i++) {
+            if (Character.isDigit((voucherCode.charAt(i)))) {
+                num+=1;
+            }
+        }
+        if (num!=8) {
+            return "REJECTED";
+        }
+        return "SUCCESS";
+    }
+
+    public String verifyTransfer() {
+        String bankName = this.paymentData.get("bankName");
+        String referenceCode = this.paymentData.get("referenceCode");
+
+        if (bankName==null | Objects.equals(bankName, "") | referenceCode==null | Objects.equals(referenceCode, "")) {
+            return "REJECTED";
+        }
+        return "SUCCESS";
+    }
+
+    public void updateStatus() {
+        if (this.method.equals("VOUCHER_CODE")) {
+            if (! this.paymentData.containsKey("voucherCode")) {
+                throw new IllegalArgumentException("Cannot proceed payment");
+            }
+            this.status = verifyCode();
+        }
+        else if (this.method.equals("BANK_TRANSFER")) {
+            if (! this.paymentData.containsKey("voucherCode")) {
+                throw new IllegalArgumentException("Cannot proceed payment");
+            }
+            this.status = verifyTransfer();
+        }
     }
 }
